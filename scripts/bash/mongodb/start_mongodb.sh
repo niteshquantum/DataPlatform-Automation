51f54f5ea938f6832d962d3a7e6b8ebb80447011
niteshquantum <nitesh.patel@quantumatix.in>
2026-06-18 23:21:18 +0530

    added install mongodb.sh

---
+++ scripts/bash/mongodb/start_mongodb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+source "$(dirname "$0")/../common/set_project_root.sh"
+
+CONFIG_FILE="$PROJECT_ROOT/config/ubuntu/mongodb.conf"
+
+MONGODB_PORT=$(grep "^MONGODB_PORT=" "$CONFIG_FILE" | cut -d'=' -f2)
+
+MONGODB_HOME="$PROJECT_ROOT/databases/mongodb/server"
+
+DATA_DIR="$PROJECT_ROOT/databases/mongodb/data"
+
+LOG_DIR="$PROJECT_ROOT/databases/mongodb/logs"
+
+LOG_FILE="$LOG_DIR/mongod.log"
+
+echo
+echo "====================================="
+echo "STARTING MONGODB"
+echo "====================================="
+echo
+
+mkdir -p "$DATA_DIR"
+mkdir -p "$LOG_DIR"
+
+"$MONGODB_HOME/bin/mongod" \
+    --dbpath "$DATA_DIR" \
+    --logpath "$LOG_FILE" \
+    --fork \
+    --port "$MONGODB_PORT"
+
+sleep 5
+
+echo
+echo "====================================="
+echo "MONGODB STARTED SUCCESSFULLY"
+echo "====================================="
+echo
+
+exit 0
