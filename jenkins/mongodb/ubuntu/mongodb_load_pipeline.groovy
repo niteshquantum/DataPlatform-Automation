07dc842c280ed950489a85656182826aee0cd729
niteshquantum <nitesh.patel@quantumatix.in>
2026-06-19 01:32:49 +0530

    added jenkin scrip for mongodb ubantu

---
+++ jenkins/mongodb/ubuntu/mongodb_load_pipeline.groovy
@@ -0,0 +1,49 @@
+pipeline {
+
+    agent any
+
+    stages {
+
+        stage('Set Permissions') {
+            steps {
+                sh '''
+                chmod +x scripts/bash/common/*.sh
+                chmod +x scripts/bash/mongodb/*.sh
+                '''
+            }
+        }
+
+        stage('Install Python Requirements') {
+            steps {
+                sh './scripts/bash/common/install_python_requirements.sh'
+            }
+        }
+
+        stage('Load MongoDB Data') {
+            steps {
+                sh './scripts/bash/mongodb/load_data.sh'
+            }
+        }
+
+        stage('Validate Loaded Data') {
+            steps {
+                sh './scripts/bash/mongodb/validate_loaded_data.sh'
+            }
+        }
+    }
+
+    post {
+
+        success {
+            echo 'UBUNTU MONGODB LOAD SUCCESSFUL'
+        }
+
+        failure {
+            echo 'UBUNTU MONGODB LOAD FAILED'
+        }
+
+        always {
+            echo 'UBUNTU MONGODB LOAD PIPELINE COMPLETED'
+        }
+    }
+}
