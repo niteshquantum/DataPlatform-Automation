49eebe519f56016b4e4587f2e5d7e88475e33dbb
niteshquantum <nitesh.patel@quantumatix.in>
2026-06-18 15:47:04 +0530

    Updated gitignore for tools and databases

--- scripts/bash/common/install_tools.sh
+++ scripts/bash/common/install_tools.sh
@@ -5,6 +5,8 @@ set -e
 source "$(dirname "$0")/set_project_root.sh"
 
 bash "$PROJECT_ROOT/scripts/bash/common/install_terraform.sh"
+bash "$PROJECT_ROOT/scripts/bash/common/install_liquibase.sh"
+bash "$PROJECT_ROOT/scripts/bash/common/install_mysql_driver.sh"
 
 echo
 echo "====================================="
