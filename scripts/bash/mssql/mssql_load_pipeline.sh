d0d9108732ec5111680a26e37fccdcbad80aefc1
niteshquantum <nitesh.patel@quantumatix.in>
2026-06-19 16:26:26 +0530

    final validate

--- scripts/bash/mssql/mssql_load_pipeline.sh
+++ scripts/bash/mssql/mssql_load_pipeline.sh
@@ -6,15 +6,10 @@ source "$(dirname "$0")/../common/set_project_root.sh"
 
 bash "$PROJECT_ROOT/scripts/bash/common/validate_python_runtime.sh"
 
-bash "$PROJECT_ROOT/scripts/bash/common/install_mssql_driver.sh"
-
 bash "$PROJECT_ROOT/scripts/bash/mssql/validate_tools.sh"
 
 bash "$PROJECT_ROOT/scripts/bash/mssql/validate_mssql.sh"
 
-bash "$PROJECT_ROOT/scripts/bash/mssql/validate_csv.sh"
-
 bash "$PROJECT_ROOT/scripts/bash/mssql/load_data.sh"
 
 bash "$PROJECT_ROOT/scripts/bash/mssql/validate_loaded_data.sh"
-
