7b8c0634b72c04fa35bcc9804d588e9b58d63497
niteshquantum <nitesh.patel@quantumatix.in>
2026-06-25 15:28:51 +0530

    update ubantu mysql

--- scripts/bash/mysql/mysql_setup_pipeline.sh
+++ scripts/bash/mysql/mysql_setup_pipeline.sh
@@ -7,6 +7,9 @@ PROJECT_ROOT=$(pwd)
 bash "$PROJECT_ROOT/scripts/bash/common/validate_python_runtime.sh"
 bash "$PROJECT_ROOT/scripts/bash/common/validate_java_runtime.sh"
 
+bash "$PROJECT_ROOT/scripts/bash/mysql/setup/install_python_requirements.sh"
+bash "$PROJECT_ROOT/scripts/bash/mysql/setup/validate_python_requirements.sh"
+
 bash "$PROJECT_ROOT/scripts/bash/common/install_terraform.sh"
 bash "$PROJECT_ROOT/scripts/bash/common/install_liquibase.sh"
 bash "$PROJECT_ROOT/scripts/bash/common/install_mysql_driver.sh"
