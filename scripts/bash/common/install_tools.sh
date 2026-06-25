f6ab47baf8f1f99c43564eafd2f4ab0625ba142e
TanishaPanwar25 <tanisha.panwar@quantumatix.in>
2026-06-25 17:47:29 +0530

    Update install_tools.sh

--- scripts/bash/common/install_tools.sh
+++ scripts/bash/common/install_tools.sh
@@ -7,6 +7,7 @@ source "$(dirname "$0")/set_project_root.sh"
 bash "$PROJECT_ROOT/scripts/bash/common/install_terraform.sh"
 bash "$PROJECT_ROOT/scripts/bash/common/install_liquibase.sh"
 bash "$PROJECT_ROOT/scripts/bash/common/install_mysql_driver.sh"
+bash "$PROJECT_ROOT/scripts/bash/common/install_mssql_driver.sh"
 
 echo
 echo "====================================="
