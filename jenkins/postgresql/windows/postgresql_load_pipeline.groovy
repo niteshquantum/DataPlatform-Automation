5717cb81dee48a374cd818a4e7f24ba867b5b5d6
Tanishapanwar25 <tanisha.panwar@quantumatix.in>
2026-07-07 14:39:39 +0530

    Add load groovy file

---
+++ jenkins/postgresql/windows/postgresql_load_pipeline.groovy
@@ -0,0 +1,58 @@
+pipeline {
+
+    agent any
+
+    stages {
+
+        stage('Validate Python Runtime') {
+            steps {
+                bat 'scripts\\batch\\common\\validate_python_runtime.bat'
+            }
+        }
+
+        stage('Validate Python Requirements') {
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\validate_python_requirements.bat'
+            }
+        }
+
+        stage('Start PostgreSQL Service') {
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\start_postgresql.bat'
+            }
+        }
+
+        stage('Validate PostgreSQL ') {
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\validate_postgresql.bat'
+            }
+        }
+
+        stage('Load Data') {
+            steps {
+                bat 'scripts\\batch\\postgresql\\load\\load_data.bat'
+            }
+        }
+
+        stage('Validate Loaded Data') {
+            steps {
+                bat 'scripts\\batch\\postgresql\\load\\validate_loaded_data.bat'
+            }
+        }
+    }
+
+    post {
+
+        success {
+            echo 'POSTGRESQL LOAD SUCCESSFUL'
+        }
+
+        failure {
+            echo 'POSTGRESQL LOAD FAILED'
+        }
+
+        always {
+            echo 'POSTGRESQL LOAD PIPELINE COMPLETED'
+        }
+    }
+}
