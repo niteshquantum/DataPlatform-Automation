24601bc88a1a6cfa1738a9c3964f793c3c533bbe
TanishaPanwar25 <tanisha.panwar@quantumatix.in>
2026-07-09 15:56:48 +0530

    Update setup_pipeline.groovy

--- jenkins/mongodb/windows/setup_pipeline.groovy
+++ jenkins/mongodb/windows/setup_pipeline.groovy
@@ -1,9 +1,45 @@
-
 pipeline {
 
     agent any
 
+    options {
+        disableConcurrentBuilds()
+    }
+
+    environment {
+        HAS_ADMIN_PRIVILEGES = 'false'
+    }
+
     stages {
+
+        stage('Check Administrator Privileges') {
+            steps {
+                script {
+
+                    def adminStatus = bat(
+                        script: 'scripts\\batch\\common\\check_admin_privileges.bat',
+                        returnStatus: true
+                    )
+
+                    env.HAS_ADMIN_PRIVILEGES =
+                        (adminStatus == 0) ? 'true' : 'false'
+
+                    echo "HAS_ADMIN_PRIVILEGES = ${env.HAS_ADMIN_PRIVILEGES}"
+
+                    if (env.HAS_ADMIN_PRIVILEGES == 'true') {
+
+                        echo 'Administrator privileges available.'
+                        echo 'Global Mongosh and MongoDB Service configuration will be enabled.'
+
+                    } else {
+
+                        echo 'Administrator privileges not available.'
+                        echo 'Global Mongosh and MongoDB Service configuration will be skipped.'
+                        echo 'MongoDB will run using project-local mode.'
+                    }
+                }
+            }
+        }
 
         stage('Validate Python Runtime') {
             steps {
@@ -48,14 +84,41 @@ pipeline {
         }
 
         stage('Configure Global Mongosh') {
+
+            when {
+                expression {
+                    env.HAS_ADMIN_PRIVILEGES == 'true'
+                }
+            }
+
             steps {
                 bat 'scripts\\batch\\mongodb\\setup\\configure_global_mongosh.bat'
             }
         }
 
         stage('Configure MongoDB Service') {
+
+            when {
+                expression {
+                    env.HAS_ADMIN_PRIVILEGES == 'true'
+                }
+            }
+
             steps {
                 bat 'scripts\\batch\\mongodb\\setup\\configure_mongodb_service.bat'
+            }
+        }
+
+        stage('Start MongoDB') {
+
+            when {
+                expression {
+                    env.HAS_ADMIN_PRIVILEGES != 'true'
+                }
+            }
+
+            steps {
+                bat 'scripts\\batch\\mongodb\\setup\\start_mongodb.bat'
             }
         }
 
@@ -64,13 +127,27 @@ pipeline {
                 bat 'scripts\\batch\\mongodb\\setup\\validate_mongodb.bat'
             }
         }
-
     }
 
     post {
 
         success {
+
             echo 'MONGODB SETUP SUCCESSFUL'
+
+            script {
+
+                if (env.HAS_ADMIN_PRIVILEGES == 'true') {
+
+                    echo 'MongoDB Windows Service configured.'
+                    echo 'Global Mongosh configuration completed.'
+
+                } else {
+
+                    echo 'MongoDB configured in project-local mode.'
+                    echo 'Windows Service and Global Mongosh skipped because Administrator privileges were unavailable.'
+                }
+            }
         }
 
         failure {
