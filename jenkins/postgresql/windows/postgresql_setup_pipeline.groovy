3840b984428c8e57b165a96d0273449449000a3a
TanishaPanwar25 <tanisha.panwar@quantumatix.in>
2026-07-09 12:33:44 +0530

    Update postgresql_setup_pipeline.groovy

--- jenkins/postgresql/windows/postgresql_setup_pipeline.groovy
+++ jenkins/postgresql/windows/postgresql_setup_pipeline.groovy
@@ -6,7 +6,33 @@ pipeline {
         disableConcurrentBuilds()
     }
 
+    environment {
+        HAS_ADMIN_PRIVILEGES = 'false'
+    }
+
     stages {
+
+        stage('Check Administrator Privileges') {
+            steps {
+                script {
+                    def adminStatus = bat(
+                        script: 'scripts\\batch\\common\\check_admin_privileges.bat',
+                        returnStatus: true
+                    )
+
+                    if (adminStatus == 0) {
+                        env.HAS_ADMIN_PRIVILEGES = 'true'
+                        echo 'Administrator privileges available.'
+                        echo 'Windows Service and Global PSQL configuration will be enabled.'
+                    } else {
+                        env.HAS_ADMIN_PRIVILEGES = 'false'
+                        echo 'Administrator privileges not available.'
+                        echo 'Windows Service and Global PSQL configuration will be skipped.'
+                        echo 'PostgreSQL will run using project-local configuration.'
+                    }
+                }
+            }
+        }
 
         stage('Validate Python Runtime') {
             steps {
@@ -38,41 +64,78 @@ pipeline {
             }
         }
 
-       stage('Deploy PostgreSQL') {
-                steps {
-                    bat 'scripts\\batch\\postgresql\\setup\\deploy_postgresql.bat'
+        stage('Deploy PostgreSQL') {
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\deploy_postgresql.bat'
+            }
+        }
+
+        stage('Configure PostgreSQL Service') {
+
+            when {
+                expression {
+                    env.HAS_ADMIN_PRIVILEGES == 'true'
                 }
             }
-            
-          stage('Start PostgreSQL') {
-    steps {
-        bat 'scripts\\batch\\postgresql\\setup\\start_postgresql.bat'
-    }
-}
-            
-            stage('Create Database') {
-                steps {
-                    bat 'scripts\\batch\\postgresql\\setup\\create_database.bat'
+
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\configure_postgresql_service.bat'
+            }
+        }
+
+        stage('Start PostgreSQL') {
+
+            when {
+                expression {
+                    env.HAS_ADMIN_PRIVILEGES == 'false'
                 }
             }
-            
-            stage('Configure Global PSQL') {
-                steps {
-                    bat 'scripts\\batch\\postgresql\\setup\\configure_global_psql.bat'
+
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\start_postgresql.bat'
+            }
+        }
+
+        stage('Create Database') {
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\create_database.bat'
+            }
+        }
+
+        stage('Configure Global PSQL') {
+
+            when {
+                expression {
+                    env.HAS_ADMIN_PRIVILEGES == 'true'
                 }
             }
-            
-            stage('Validate Environment') {
-                steps {
-                    bat 'scripts\\batch\\postgresql\\setup\\validate_environment.bat'
-                }
+
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\configure_global_psql.bat'
             }
+        }
+
+        stage('Validate Environment') {
+            steps {
+                bat 'scripts\\batch\\postgresql\\setup\\validate_environment.bat'
+            }
+        }
     }
 
     post {
 
         success {
             echo 'POSTGRESQL SETUP SUCCESSFUL'
+
+            script {
+                if (env.HAS_ADMIN_PRIVILEGES == 'true') {
+                    echo 'PostgreSQL Windows Service configured.'
+                    echo 'Global PSQL configuration completed.'
+                } else {
+                    echo 'PostgreSQL configured in project-local mode.'
+                    echo 'Windows Service and Global PSQL skipped because Administrator privileges were unavailable.'
+                }
+            }
         }
 
         failure {
