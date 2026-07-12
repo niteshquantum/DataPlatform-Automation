f83070b5cdf1a194fdd661ed14af9fda39ec144b
niteshquantum <nitesh.patel@quantumatix.in>
2026-07-12 19:15:15 +0530

    update setup.sh

--- scripts/bash/mssql/setup/validate_python_requirements.sh
+++ scripts/bash/mssql/setup/validate_python_requirements.sh
@@ -12,6 +12,13 @@ echo
 
 python3 --version
 
+# Ensure basic system unixodbc is present so pyodbc can bind its libraries successfully
+if ! dpkg -l unixodbc 2>/dev/null | grep -q '^ii'
+then
+    echo "Installing prerequisite system unixodbc library..."
+    sudo apt-get update && sudo apt-get install -y unixodbc
+fi
+
 python3 -c "import yaml"
 python3 -c "import dotenv"
 python3 -c "import pyodbc"
