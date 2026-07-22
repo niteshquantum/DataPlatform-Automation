225d027573e2532f21a919a2eb934bff72ca37af
niteshquantum <nitesh.patel@quantumatix.in>
2026-07-22 09:07:58 +0530

    fix: wrap env assignment in script block for Declarative Pipeline compliance

--- jenkins/postgresql/windows/load_pipeline.groovy
+++ jenkins/postgresql/windows/load_pipeline.groovy
@@ -61,16 +61,20 @@ pipeline {
 
             steps {
 
-                bat """
-                    python scripts\\logging\\logger.py init ^
-                    --database postgresql ^
-                    --action load ^
-                    --os windows ^
-                    --build-number "${env.BUILD_NUMBER}" ^
-                    --job-name "${env.JOB_NAME}" ^
-                    --build-url "${env.BUILD_URL}"
-                """
-                env.POSTGRESQL_LOAD_LOGGING_INITIALIZED = 'true'
+                script {
+
+                    bat """
+                        python scripts\\logging\\logger.py init ^
+                        --database postgresql ^
+                        --action load ^
+                        --os windows ^
+                        --build-number "${env.BUILD_NUMBER}" ^
+                        --job-name "${env.JOB_NAME}" ^
+                        --build-url "${env.BUILD_URL}"
+                    """
+
+                    env.POSTGRESQL_LOAD_LOGGING_INITIALIZED = 'true'
+                }
             }
         }
 
